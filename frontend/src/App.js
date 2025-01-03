e94019cd736e03dc46ff303dee2aa596fd44fc00
Ars003 <aditya003rs@gmail.com>
2025-01-03 13:10:24 +0530

    Added frontend folder as part of the main repository

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './App.css';
+import Home from './components/Home';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import { Toaster } from 'react-hot-toast';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Toaster position="top-center" />
+        <Header />
+
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
