5f0a3acd467a23bedf4933d35fcef95e06737b84
Ars003 <aditya003rs@gmail.com>
2026-05-08 00:47:31 +0530

    Resolved merge conflicts

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,30 +1,21 @@
 import express from 'express';
-<<<<<<< HEAD
 import { getProducts, newProduct, getSingleProduct, updateSingleProduct, deleteSingleProduct, createProductReview, getProductReview, deleteReview } from '../controllers/productControllers.js';
-=======
 import { getProducts, getAdminProducts, newProduct, getSingleProduct, updateSingleProduct, deleteSingleProduct, createProductReview, getProductReview, deleteReview, canUserReview, uploadProductImages } from '../controllers/productControllers.js';
->>>>>>> 998ca10ab58964e6ded975519753f82723124b1a
 import { authorizeRoles, isAuthenticatedUser } from '../middlewares/auth.js';
 const router = express.Router();
 
 router.route('/products').get(getProducts);
-<<<<<<< HEAD
 router.route('/admin/products').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 router.route('/products/:id').get(getSingleProduct);
-=======
 router.route('/admin/products').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct)
 .get(isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
 router.route('/products/:id').get(getSingleProduct);
 router.route('/admin/products/:id/upload_images').put(isAuthenticatedUser, authorizeRoles('admin'), uploadProductImages)
->>>>>>> 998ca10ab58964e6ded975519753f82723124b1a
 router.route('/admin/products/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateSingleProduct);
 router.route('/admin/products/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteSingleProduct);
 router.route('/reviews').put(isAuthenticatedUser, createProductReview).get(isAuthenticatedUser, getProductReview);
 router.route('/admin/reviews').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteReview);
-<<<<<<< HEAD
-=======
 router.route('/can_review').get(isAuthenticatedUser, canUserReview);
->>>>>>> 998ca10ab58964e6ded975519753f82723124b1a
 
 
 export default router;   
